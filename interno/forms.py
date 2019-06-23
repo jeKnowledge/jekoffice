481fe53f70c75f87db3bbc763afc065dc4c594c0
Bernardo Prior <bernardo.prior@hotmail.com>
2019-06-23 21:25:02 +0100

    fix

--- interno/forms.py
+++ interno/forms.py
@@ -1,29 +1,15 @@
 from django import forms
 from .models import Relatorios_recrutamento
 from users.models import CustomUser
+from django import forms
 
-def USERS_CHOICES():
-    try:
-        users = CustomUser.objects.all()
-        escolhas = (('NADA','NADA'),)
-        if users:
-            for user in users:
-                tuplo = ((user.username,user.first_name + ' ' + user.last_name),)
-                try:
-                    escolhas += tuplo
-                except:
-                    escolhas = tuplo
-            return escolhas
-    except:
-        return (('NADA','NADA'),)
-    return (('NADA','NADA'),)
 
 class RecrutamentoForm(forms.Form):
     SEMESTRE_ESCOLHA = (("1º Semestre","1º Semestre"), ("2º Semestre","2º Semestre"))
 
 
     semestre = forms.ChoiceField(choices = SEMESTRE_ESCOLHA, label = 'Semestre')
-    responsavel = forms.ChoiceField(choices = USERS_CHOICES())
+    responsavel = forms.ModelChoiceField(queryset=CustomUser.objects.all(),widget=forms.Select,empty_label=None)
     n_vagas_tec = forms.IntegerField(label = 'Nº vagas TEC:')
     n_vagas_ino = forms.IntegerField(label = 'Nº vagas INO:')
     n_vagas_int = forms.IntegerField(label = 'Nº vagas INT:')
